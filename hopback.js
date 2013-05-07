f8fd874a8a0c23faf071eea8b3eaa24d70f6b936
svanderbleek <sandy.vanderbleek@gmail.com>
2013-05-07 13:31:06 -0700

    don't check ready before auth

--- hopback.js
+++ hopback.js
@@ -2,7 +2,9 @@ var Redis = require('redis'),
   Url = require('url');
 
 redisUrl = Url.parse(process.env.REDISTOGO_URL);
-redisClient = Redis.createClient(redisUrl.port, redisUrl.hostname);
+redisClient = Redis.createClient(redisUrl.port, redisUrl.hostname, {
+  no_ready_check: true
+});
 
 redisClient.auth(redisUrl.auth.split(':')[1], popQueue);
 
